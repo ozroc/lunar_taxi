64627682ea89fd770d1f3ab723d4b85824004e9d
Int-0 <tobias.deb@gmail.com>
2013-09-08 19:06:38 +0200

    Scroll test OK.

--- game.py
+++ game.py
@@ -2,43 +2,86 @@
 
 import pygame
 
+DIR_STOP = 0
+DIR_UP = 1
+DIR_DOWN = 2
+DIR_LEFT = 3
+DIR_RIGHT = 4
+
 class Camera(object):
-    def __init__(self, aim=(0, 0), zoom=1.0, viewport=None):
-        self.__aim = aim
-        self.__zoom = zoom
+    def __init__(self, space):
+        self.space = space
+        self.__speed = 8
+        self.__offset = (0, 0)
 
-        self.__viewport = None
+        screen = pygame.display.get_surface()
+        self.__image = pygame.Surface(screen.get_size())
+        self.__image.blit(self.space.image.subsurface(pygame.Rect(self.__offset,
+                                                                  self.__image.get_size())), self.__offset)
+
+    def move_camera(self, direction):
+        view_size = self.__image.get_size()
+        src_size = self.space.image.get_size()
+
+        if direction == DIR_LEFT:
+            if self.__offset[0] - self.__speed < 0:
+                self.__offset = (0, self.__offset[1])
+            else:
+                self.__offset = (self.__offset[0] - self.__speed,
+                                 self.__offset[1])
+
+        elif direction == DIR_RIGHT:
+            if ((self.__offset[0] + view_size[0] + self.__speed) > src_size[0]):
+                self.__offset = (src_size[0] - view_size[0], self.__offset[1])
+            else:
+                self.__offset = (self.__offset[0] + self.__speed,
+                                 self.__offset[1])
+
+        elif direction == DIR_UP:
+            if self.__offset[1] - self.__speed < 0:
+                self.__offset = (self.__offset[0], 0)
+            else:
+                self.__offset = (self.__offset[0],
+                                 self.__offset[1] - self.__speed)
+
+        elif direction == DIR_DOWN:
+            if ((self.__offset[1] + view_size[1] + self.__speed) > src_size[1]):
+                self.__offset = (self.__offset[0], src_size[1] - view_size[1])
+            else:
+                self.__offset = (self.__offset[0],
+                                 self.__offset[1] + self.__speed)
+
+        src_rect = pygame.Rect(self.__offset, view_size)
+        self.__image.blit(self.space.image.subsurface(src_rect), (0, 0))
+
 
     @property
-    def offset(self):
-        return self.__aim
-
-    @property
-    def zoom(self):
-        return self.__zoom
-
-    def set_viewport(self, viewport):
-        self.__viewport = viewport
+    def view(self):
+        return self.__image
 
 
 class Game(object):
-    def __init__(self, destination, static_background=None):
-        self.__scr = destination
-        self.__background = static_background
-
-        self.__camera = Camera(viewport=self.__scr.get_size())
-
-        self.__ingame = True
+    def __init__(self, space):
+        self.space = space
 
     def update(self):
-        if self.__background:
-            self.__scr.blit(self.__background.subsurface(self.__camera),
-                            (0, 0))
-        pygame.display.update()
+        self.space.update()
 
 class Space(object):
-    def __init__(self):
+    def __init__(self, static_background=None):
+        self.background = static_background
+
+    def update(self):
         pass
+
+    @property
+    def render(self):
+        self.update()
+        return self.background
+
+    @property
+    def image(self):
+        return self.background
 
 class Planet(object):
     def __init__(self):
--- main.py
+++ main.py
@@ -12,6 +12,11 @@ try:
 except ImportError:
     import android.mixer as mixer
 
+import game
+
+def load_image(filename):
+    return pygame.image.load(filename).convert_alpha()
+
 def main():
     if android:
         android.init()
@@ -19,6 +24,52 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((800, 480))
+    
+    space = game.Space(load_image('background.jpg'))
+    camera = game.Camera(space)
+    direction = game.DIR_STOP
 
+    in_game = True
+    while in_game:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                in_game = False
+            elif (event.type == pygame.KEYDOWN and
+                  event.key == pygame.K_ESCAPE):
+                in_game = False
+            elif (event.type == pygame.KEYDOWN and
+                  event.key == pygame.K_LEFT):
+                direction = game.DIR_LEFT
+            elif (event.type == pygame.KEYDOWN and
+                  event.key == pygame.K_RIGHT):
+                direction = game.DIR_RIGHT
+            elif ((event.type == pygame.KEYUP) and
+                  (event.key == pygame.K_LEFT) and
+                  (direction == game.DIR_LEFT)):
+                direction = game.DIR_STOP
+            elif ((event.type == pygame.KEYUP) and
+                  (event.key == pygame.K_RIGHT) and
+                  (direction == game.DIR_RIGHT)):
+                direction = game.DIR_STOP
+            elif (event.type == pygame.KEYDOWN and
+                  event.key == pygame.K_UP):
+                direction = game.DIR_UP
+            elif (event.type == pygame.KEYDOWN and
+                  event.key == pygame.K_DOWN):
+                direction = game.DIR_DOWN
+            elif ((event.type == pygame.KEYUP) and
+                  (event.key == pygame.K_UP) and
+                  (direction == game.DIR_UP)):
+                direction = game.DIR_STOP
+            elif ((event.type == pygame.KEYUP) and
+                  (event.key == pygame.K_DOWN) and
+                  (direction == game.DIR_DOWN)):
+                direction = game.DIR_STOP
+
+        camera.move_camera(direction)
+        dirty = screen.blit(camera.view, (0, 0))
+        pygame.display.flip()
+    
 if __name__ == '__main__':
     main()
